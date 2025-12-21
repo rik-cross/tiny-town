cc9b86a9c25d683fa92d7b54808eb72af953eec2
Rik Cross <rik@raspberrypi.org>
2025-12-21 11:02:06 +0000

    Latest version

--- obj/Release/net8.0/MonoGameECSTest.AssemblyInfo.cs
+++ obj/Release/net8.0/MonoGameECSTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGameECSTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9d4da8bde43c1a06a7d716cd202979d0323f32e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d74b96708ec17504cdbd4d8861dc2295f6f5006")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGameECSTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGameECSTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
